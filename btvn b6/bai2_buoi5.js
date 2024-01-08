c7b83fa74be87e3f1935c79aab1450b4c4f194cf
nguyentruong032 <thesucceeder032045@gmail.com>
2024-01-08 11:12:52 +0700

    add file

---
+++ btvn b6/bai2_buoi5.js
@@ -0,0 +1,7 @@
+function chan_le(x) {
+  if (x % 2 == 0) {
+    document.write("<h1> đây là số chẵn</h1>");
+  } else {
+    document.write("<h1> đây là số lẻ</h1>");
+  }
+}
---
+++ btvn b6/bai3_buoi5.js
@@ -0,0 +1,6 @@
+function nhapso(i) {
+  do {
+    var x = prompt("Đoán xem");
+  } while (x != i);
+  document.write("Bạn đã đoán đúng");
+}
